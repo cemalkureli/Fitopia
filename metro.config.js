f19ce6e59df015d3eed567417e72a59b36596ebf
cemalkureli <cemalkureli@gmail.com>
2026-05-31 11:32:37 +0300

    fix: add glb/gltf/bin to Metro asset extensions

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('webm');
+config.resolver.assetExts.push('webm', 'glb', 'gltf', 'bin');
 module.exports = config;
